7312db9a3ee916da55103da233e550778fc78760
danielsun <danielsun12345@gmail.com>
2020-07-23 16:31:39 -0700

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,50 @@
+let counterIsPaused = false;
+const counterH1 = document.querySelector('#counter')
+
+setInterval(function(){
+    if(counterIsPaused == false){
+        counterH1.innerText++
+    }
+}, 1000)
+
+let pause = document.querySelector("#pause")
+pause.addEventListener('click', function(){
+    if(counterIsPaused == false){
+        counterIsPaused = true
+    } else {
+        counterIsPaused = false
+    }
+})
+
+let add = document.querySelector('#plus')
+add.addEventListener('click', function(){
+    counterH1.innerText = parseFloat(counterH1.innerText) + 1
+})
+
+let minus = document.querySelector('#minus')
+minus.addEventListener('click', function(){
+    counterH1.innerText = parseFloat(counterH1.innerText) - 1
+})
+
+let likes = {};
+
+let likeList = document.querySelector('.likes')
+
+let heart = document.querySelector('#heart')
+heart.addEventListener('click', function(){
+    let counterNumber = counterH1.innerText
+    if(likes[counterNumber] == undefined){
+        likes[counterNumber] = 1
+    } else {
+        likes[counterNumber]++
+    }
+
+    likeList.innerHTML = ""
+    for (var key in likes) {
+        let number = key
+        let numberOfLikes = likes[key]
+        let likeItem = document.createElement('li')
+        likeItem.innerText = `${number} has been liked ${numberOfLikes}`
+        likeList.append(likeItem)
+    }
+})
